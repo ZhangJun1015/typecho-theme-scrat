c5f88ebfa4fb2133d75408a4b8352b33944f742a
Sirit <zhangjun4u@126.com>
2019-03-26 01:53:59 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 function themeConfig($form) {
     $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
     $form->addInput($logoUrl);
-    
+  
+    $ICPbeian = new Typecho_Widget_Helper_Form_Element_Text('ICPbeian', NULL, NULL, _t('ICP备案号'), _t('在这里输入ICP备案号,留空则不显示'));
+    $form->addInput($ICPbeian);  
 }
 
 function img_postthumb($cid) {
